6d429e6301f1d1d7b5db896706a25dbdad48a255
kpchamp <kpchamp@uw.edu>
2017-10-22 17:23:04 -0700

    created code for fitting linear regression model with complex values (based on sklearn's version; not full-featured for now)

---
+++ utils/linear_model.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class ComplexLinearRegression:
+    """
+    Fit a linear regression model where the data is allowed to be complex.
+    """
+    def __init__(self, fit_intercept=True):
+        self.fit_intercept = fit_intercept
+
+    def fit(self, Xin, y):
+        X_offset = np.average(Xin, axis=0)
+        X = Xin - X_offset
+        y_offset = np.average(y, axis=0)
+        y = y - y_offset
+
+        self.coef_, self._residues, self.rank_, self.singular_ = np.linalg.lstsq(X, y)
+        self.coef_ = self.coef_.T
+
+        if self.fit_intercept:
+            self.coef_ = self.coef_
+            self.intercept_ = y_offset - np.dot(X_offset, self.coef_.T)
+        else:
+            self.intercept_ = 0.
+
+    def predict(self, Xin):
+        return np.dot(Xin, self.coef_.T) + self.intercept_
