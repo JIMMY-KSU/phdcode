92e0e9935826455dcf6e0ab25d49e4aee4e7e731
kpchamp <kpchamp@uw.edu>
2017-10-24 13:38:55 -0700

    updated jordan form function to enable putting out real form

--- methods/utils.py
+++ methods/utils.py
@@ -15,8 +15,11 @@ def hankel_matrix(Xin, delay):
     return X
 
 
-def jordan(A, real=False, threshold=1e-5):
-    J = np.zeros(A.shape, dtype=np.complex)
+def jordan(A, real=False, threshold=1e-8):
+    if real:
+        J = np.zeros(A.shape, dtype=np.complex)
+    else:
+        J = np.zeros(A.shape, dtype=np.complex)
     evals, evecs = la.eig(A)
 
     evals_idx = np.arange(evals.size)
@@ -25,6 +28,8 @@ def jordan(A, real=False, threshold=1e-5):
     while i < J.shape[0]:
         comparison = np.abs(evals_copy[0] - evals_copy) < threshold
         if np.sum(comparison) > 1:
+            if real and np.abs(np.imag(evals_copy[0])) > threshold:
+                raise ValueError('not implemented for duplicate complex eigenvalues')
             algebraic_multiplicity = np.sum(comparison)
             geometric_multiplicity = np.linalg.matrix_rank(evecs[evals_idx[comparison]])
             J[i:i+algebraic_multiplicity,i:i+algebraic_multiplicity] = np.eye(algebraic_multiplicity)*evals_copy[comparison]
@@ -33,8 +38,19 @@ def jordan(A, real=False, threshold=1e-5):
             evals_idx = evals_idx[~comparison]
             evals_copy = evals_copy[~comparison]
             i += algebraic_multiplicity
+        elif real and np.abs(np.imag(evals_copy[0])) > threshold:
+            conj_idx = np.argsort(np.abs(np.conj(evals_copy[0]) - evals_copy))[0]
+            J[i:i+1,i:i+1] = np.eye(2)*np.real(evals_copy[0])
+            J[i,i+1] = np.abs(np.imag(evals_copy[0]))
+            J[i+1,i] = -np.abs(np.imag(evals_copy[0]))
+
+            mask = np.ones(evals_idx.size, dtype=bool)
+            mask[[0,conj_idx]] = False
+            evals_idx = evals_idx[mask]
+            evals_copy = evals_copy[mask]
+            i += 2
         else:
-            J[i,i] = evals_copy[0]
+            J[i,i] = np.real(evals_copy[0])
             evals_idx = evals_idx[1:]
             evals_copy = evals_copy[1:]
             i += 1
