bce7d81b3c7ba8119afaeba7e5afaa56b34c50cb
kpchamp <kpchamp@uw.edu>
2017-11-03 16:17:26 -0700

    updating SINDyC to include option for fitting with integral instead of derivative

--- methods/sindyc.py
+++ methods/sindyc.py
@@ -1,31 +1,47 @@
 import numpy as np
 from scipy.integrate import ode
 from .sindy import pool_data
+from .utils import integrate
 
 
 class SINDyC:
-    def __init__(self, use_sine=False):
+    def __init__(self, use_sine=False, method='derivative'):
         self.use_sine = use_sine
+        self.method = method
 
     def fit(self, Xin, Uin, poly_order, dt=None, Xprime=None, coefficient_threshold=.01):
-        if Xprime is None:
+        if self.method == 'derivative':
+            if Xprime is None:
+                if dt is None:
+                    raise ValueError('must provide at least one of derivative or time step')
+                Xprime = (Xin[:,2:]-Xin[:,:-2])/(2*dt)
+                X = Xin[:,1:-1]
+                U = Uin[:,1:-1]
+            else:
+                X = Xin
+                U = Uin
+
+            LHS = Xprime
+            Thetax,labelsx = pool_data(X, poly_order, self.use_sine)
+            Thetau,labelsu = pool_data(U, poly_order, self.use_sine, include_constant=False, varname='u')
+            RHS = np.concatenate((Thetax,Thetau), axis=0)
+        elif self.method == 'integral':
             if dt is None:
-                raise ValueError('must provide at least one of derivative or time step')
-            Xprime = (Xin[:,2:]-Xin[:,:-2])/(2*dt)
-            X = Xin[:,1:-1]
-            U = Uin[:,1:-1]
+                raise ValueError('must provide time step')
+
+            LHS = Xin - np.broadcast_to(Xin[:,0], (Xin.shape[1],Xin.shape[0])).T
+
+            Thetax,labelsx = pool_data(Xin, poly_order, self.use_sine)
+            Thetau,labelsu = pool_data(Uin, poly_order, self.use_sine, include_constant=False, varname='u')
+            Theta = np.concatenate((Thetax,Thetau), axis=0)
+            RHS = integrate(Theta, dt)
         else:
-            X = Xin
-            U = Uin
+            raise ValueError('invalid fitting method')
 
-        Thetax,labelsx = pool_data(X, poly_order, self.use_sine)
-        Thetau,labelsu = pool_data(U, poly_order, self.use_sine, include_constant=False, varname='u')
-
-        Theta = np.concatenate((Thetax,Thetau), axis=0)
         self.labels = labelsx + labelsu
 
-        n,T = Xprime.shape
-        Xi = np.linalg.lstsq(Theta.T,Xprime.T)[0]
+        n,T = LHS.shape
+        Xi = np.linalg.lstsq(RHS.T,LHS.T)[0]
 
         for k in range(10):
             small_inds = (np.abs(Xi) < coefficient_threshold)
@@ -34,34 +50,47 @@ class SINDyC:
                 big_inds = ~small_inds[:,i]
                 if np.where(big_inds)[0].size == 0:
                     continue
-                Xi[big_inds,i] = np.linalg.lstsq(Theta[big_inds].T, Xprime[i])[0]
+                Xi[big_inds,i] = np.linalg.lstsq(RHS[big_inds].T, LHS[i])[0]
 
         self.poly_order = poly_order
         self.Xi = Xi
-        self.error = np.sum(np.mean((Xprime - np.dot(Xi.T,Theta))**2,axis=1))
+        self.error = np.sum(np.mean((Xprime - np.dot(Xi.T,RHS))**2,axis=1))
 
     def fit_incremental(self, Xin, Uin, dt=None, Xprime=None, coefficient_threshold=.01, error_threshold=1e-3):
-        if Xprime is None:
+        if self.method == 'derivative':
+            if Xprime is None:
+                if dt is None:
+                    raise ValueError('must provide at least one of derivative or time step')
+                Xprime = (Xin[:,2:]-Xin[:,:-2])/(2*dt)
+                X = Xin[:,1:-1]
+                U = Uin[:,1:-1]
+            else:
+                X = Xin
+                U = Uin
+        elif self.method == 'integral':
             if dt is None:
-                raise ValueError('must provide at least one of derivative or time step')
-            Xprime = (Xin[:,2:]-Xin[:,:-2])/(2*dt)
-            X = Xin[:,1:-1]
-            U = Uin[:,1:-1]
-        else:
+                raise ValueError('must provide time step')
             X = Xin
-            U = Uin
+        else:
+            raise ValueError('invalid fitting method')
 
         poly_orders = np.arange(1,6)
 
         for order in poly_orders:
             Thetax,labelsx = pool_data(X, order, self.use_sine)
             Thetau,labelsu = pool_data(U, order, self.use_sine, include_constant=False, varname='u')
-
             Theta = np.concatenate((Thetax,Thetau), axis=0)
+            if self.method == 'derivative':
+                RHS = Theta
+                LHS = Xprime
+            else:
+                RHS = integrate(Theta, dt)
+                LHS = X - np.broadcast_to(X[:,0], (X.shape[1],X.shape[0])).T
+
             self.labels = labelsx + labelsu
 
-            n,T = Xprime.shape
-            Xi = np.linalg.lstsq(Theta.T,Xprime.T)[0]
+            n,T = LHS.shape
+            Xi = np.linalg.lstsq(RHS.T,LHS.T)[0]
 
             for k in range(10):
                 small_inds = (np.abs(Xi) < coefficient_threshold)
@@ -70,9 +99,9 @@ class SINDyC:
                     big_inds = ~small_inds[:,i]
                     if np.where(big_inds)[0].size == 0:
                         continue
-                    Xi[big_inds,i] = np.linalg.lstsq(Theta[big_inds].T, Xprime[i])[0]
+                    Xi[big_inds,i] = np.linalg.lstsq(RHS[big_inds].T, LHS[i])[0]
 
-            error = np.sum(np.mean((Xprime - np.dot(Xi.T,Theta))**2,axis=1))
+            error = np.sum(np.mean((LHS - np.dot(Xi.T,RHS))**2,axis=1))
             print("order %d, error %f" % (order, error))
             if error < error_threshold:
                 break
