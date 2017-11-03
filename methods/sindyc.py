updating SINDyC to include option for fitting with integral instead of derivative; small bug fix

methods/sindyc.py
@@ -54,7 +54,7 @@ class SINDyC:
 
         self.poly_order = poly_order
         self.Xi = Xi
-        self.error = np.sum(np.mean((Xprime - np.dot(Xi.T,RHS))**2,axis=1))
+        self.error = np.sum(np.mean((LHS - np.dot(Xi.T,RHS))**2,axis=1))
 
     def fit_incremental(self, Xin, Uin, dt=None, Xprime=None, coefficient_threshold=.01, error_threshold=1e-3):
         if self.method == 'derivative':