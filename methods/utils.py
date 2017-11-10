f4f1c659a7e61fb370b0e4aeb0f150b8e759cfd1
kpchamp <kpchamp@uw.edu>
2017-11-10 11:34:13 -0800

    updated differentiation and integration for nonuniform time sampling

--- methods/utils.py
+++ methods/utils.py
@@ -15,6 +15,42 @@ def hankel_matrix(Xin, delay):
     return X
 
 
+def differentiate(X, t, method='centered_difference', dt_max=None):
+    if t.size == 1:
+        # if t is a single number, assume uniform sampling with time step t
+        return (X[:,2:]-X[:,:-2])/(2*t)
+    else:
+        # if t is an array, may have nonuniform sampling
+        X_diff = X[:,2:] - X[:,:-2]
+        t_diff = t[2:] - t[:-2]
+        if dt_max is None:
+            return X_diff*(1/t_diff)
+        else:
+            valid_idx = np.where(t_diff < dt_max)[0]
+            return X_diff[:,valid_idx]*(1/t_diff[valid_idx])
+
+
+def integrate(X, t, dt_max=None):
+    X_int = np.zeros(X.shape)
+    if t.size == 1:
+        # uniform time step
+        X_diff = t/2*(X[:,1:] + X[:,:-1])
+        X_int[:,1:] = np.cumsum(X_diff, axis=1)
+    else:
+        # nonuniform time step
+        X_diff = (X[:,1:] + X[:,:-1])/2
+        t_diff = t[1:] - t[:-1]
+        if dt_max is None:
+            X_int[:,1:] = np.cumsum(X_diff*t_diff, axis=1)
+        else:
+            invalid_idx = np.where(t_diff > dt_max)[0]
+            X_int[:,1:invalid_idx[0]] = np.cumsum(X_diff[:,:invalid_idx[0]-1]*t_diff[:invalid_idx[0]-1], axis=1)
+            for i,idx in invalid_idx:
+                X_int[:,invalid_idx[i]+1:invalid_idx[i+1]] = np.cumsum(X_diff[:,invalid_idx[i]:invalid_idx[i+1]-1] \
+                                                                     *t_diff[invalid_idx[i]:invalid_idx[i+1]-1], axis=1)
+    return X_int
+
+
 def jordan(A, real=False, threshold=1e-8, return_transition_matrix=False):
     if real:
         J = np.zeros(A.shape)
@@ -88,10 +124,3 @@ def jordan(A, real=False, threshold=1e-8, return_transition_matrix=False):
         return J,P
     else:
         return J
-
-
-def integrate(X, dt):
-    X_diff = dt/2*(X[:,1:] + X[:,:-1])
-    X_int = np.zeros(X.shape)
-    X_int[:,1:] = np.cumsum(X_diff, axis=1)
-    return X_int
