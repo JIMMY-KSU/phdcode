63016388679b9585b93f19c262cd27e96b99803c
kpchamp <kpchamp@uw.edu>
2017-11-26 16:05:09 -0800

    debugging time delay DMD with option for different spacing between delay coordinates

--- methods/utils.py
+++ methods/utils.py
@@ -9,9 +9,9 @@ def unit_circle(spacing=.01):
 def hankel_matrix(Xin, n_delay_coordinates, spacing=1):
     n_inputs, n_samples = Xin.shape
 
-    X = np.zeros((n_inputs * (n_delay_coordinates + 1), n_samples - n_delay_coordinates - 1))
+    X = np.zeros((n_inputs * (n_delay_coordinates + 1), int(np.floor((n_samples - n_delay_coordinates - 1)/spacing))))
     for i in range(n_delay_coordinates+1):
-        idxs = spacing*np.arange(i, int((i + n_samples - n_delay_coordinates - 1)/spacing))
+        idxs = spacing*np.arange(i, int(np.floor((i + n_samples - n_delay_coordinates - 1)/spacing)))
         # X[i + (n_delay_coordinates + 1) * np.arange(n_inputs)] = Xin[:, i:i + n_samples - n_delay_coordinates - 1]
         X[i + (n_delay_coordinates + 1) * np.arange(n_inputs)] = Xin[:, idxs]
     return X
