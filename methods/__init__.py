d8d06c02c99cf969277b46a0033917071246bc84
kpchamp <kpchamp@uw.edu>
2018-01-06 10:32:56 -0800

    updated init file

--- methods/__init__.py
+++ methods/__init__.py
@@ -4,4 +4,4 @@ from .dmdc import DMDc
 from .sindy import pool_data, sindy_setup, SINDy
 from .sindyc import SINDyC
 
-from .utils import *
+from .utils import unit_circle, hankel_matrix, differentiate, integrate, jordan
