16451bf1eb13e338734f9dcdfd772410156c65a3
kpchamp <kpchamp@uw.edu>
2018-01-06 10:38:49 -0800

    updated init files

---
+++ examples/__init__.py
@@ -0,0 +1,19 @@
+from .oscillators import simulate_linear_oscillator
+from .oscillators import simulate_vanderpol_oscillator
+from .oscillators import simulate_duffing_oscillator
+from .oscillators import simulate_coupled_linear
+from .oscillators import simulate_coupled_vdp
+from .oscillators import simulate_coupled_duffing
+from .oscillators import simulate_coupled_vdp_duffing
+from .oscillators import simulate_coupled_linear_duffing
+from .oscillators import simulate_coupled_linear_vdp
+from .oscillators import simulate_coupled_vdp_lorenz
+
+from .lorenz import simulate_lorenz
+from .lorenz import simulate_coupled_lorenz
+from .lorenz import simulate_lorenz96
+from .lorenz import simulate_coupled_lorenz96
+
+from .utils import generate_circle_image
+from .utils import generate_gaussian_image
+from .utils import generate_square_image
---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+from .linear_model import ComplexLinearRegression
+from .optimal_svht_coef import optimal_svht_coef
+from .plotting import centered_colormap, viridis_baseline
+from .sampling import multiscale_timesample, multiscale_time_to_subindex, uniform_time_to_index
+from .tvregdiff import TVRegDiff
+from .utils import partitions
